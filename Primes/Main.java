b7b9876f5dc5bbd6d2cddba2ba4d74459e1063e4
Ben Brannick <ben.brannick13@imperial.ac.uk>
2016-02-09 13:55:26 +0000

    Improve sumOfTwoPrimes

--- Primes/Main.java
+++ Primes/Main.java
@@ -6,11 +6,14 @@ public class Main {
       int n = Integer.parseInt(args[1]);
       if (args[0].equals("0")) {
 
-        System.out.println(n + " is " + (Primes.isPrime(n) ? "" : "not ") + "prime");
+        System.out.println(n + " is " + 
+                           (Primes.isPrime(n) ? "" : "not ") + "prime");
 
       } else {
 
-        System.out.println(n + " is " + (Primes.sumOfTwoPrimes(n) ? "" : "not ") + "the sum of two primes");
+        System.out.println(n + " is " + 
+                           (Primes.sumOfTwoPrimes(n) ? "" : "not ") + 
+                           "the sum of two primes");
 
       }
     }
--- Primes/Primes.java
+++ Primes/Primes.java
@@ -1,16 +1,28 @@
 public class Primes {
 
+  /*
+  / This method works as a primefilter.
+  / All numbers can be represented as 6k+n, but for a number to be prime,
+  / it must be represented by 6k +/- 1, since 6k, 6k+2 and 6k+4 all divide by 2, 
+  / and 6k+3 divides by 3.
+  / This doesn't return only primes, but since the only non-primes it generates
+  / are multiples of a previous prime, it works for checking primality.
+  */
   public static int getPrime(int n) {
     if (n == 0) return 2;
     if (n == 1) return 3;
     return 6 * (n / 2) + (n % 2 == 0 ? -1 : 1);
   }
 
+  /*
+  / Here, if a number is greater than 2, and isn't even, we take every prime
+  / lower than the square root of n, and check whether n divides exactly into it.
+  */
   public static boolean isPrime(int n) {
     if (n < 2) return false;
     if (n > 2 && n % 2 == 0) return false;
-    int i = 0;
-    int prime = getPrime(0);
+    int i = 1;
+    int prime = getPrime(1);
     while (prime < Math.sqrt(n)) {
       if (n % prime == 0) return false;
       prime = getPrime(++i);
@@ -18,11 +30,19 @@ public class Primes {
     return true;
   }
 
+  /*
+  / This function checks every prime number less than half n, and checks whether
+  / the difference between n and that prime is also prime.
+  */
   public static boolean sumOfTwoPrimes(int n) {
-    for (int i = 1; i <= n / 2; i++) {
-      if (isPrime(i) && isPrime(n-i)) {
+    int i = 0;
+    int prime = getPrime(i);
+    while (prime <= n/2) {
+      if (isPrime(n-prime)) {
+        System.out.printf("%d is the sum of %d and %d\n", n, prime, n-prime);
         return true;
       }
+      prime = getPrime(++i);
     }
     return false;
   }
